448ad146aba3ecf801a6ba67942352ae3e7ec875
oclockvn <oclockvn@gmail.com>
2017-06-01 13:01:13 +0700

    added item component

---
+++ app/components/item/item.component.js
@@ -0,0 +1,16 @@
+(function() {
+
+    function itemController() {
+        var self = this;
+    }
+
+    angular.module("app")
+        .component("item", {
+            templateUrl: "app/components/item/item.template.html",
+            controller: [itemController],
+            parent: "^^wallet",
+            bindings: {
+                
+            }
+        });
+})();
